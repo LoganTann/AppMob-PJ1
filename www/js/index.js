ff8a3176828ffc105503753f04a36509cdd83bfe
LoganTann <logon313@hotmail.fr>
2022-02-18 17:18:13 +0100

    feat: suppression pour react native TP2

--- www/js/index.js
+++
@@ -1,8 +0,0 @@
-alert("Je suis là");
-
-document.addEventListener("DOMContentLoaded", function() {
-    const content = document.querySelector(".content");
-    content.addEventListener("click", function() {
-        alert("Je suis cliqué");
-    });
-});
